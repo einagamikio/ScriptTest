5e965344bacc492bbe13519b94dd0469851cd772
榮長幹雄 <0419mikio@gmail.com>
2017-07-03 14:17:10 +0900

    Lesson4の課題提出
    配列を宣言して出力/Bossクラスに、変数mpとMagic関数を作成してStart関数から呼び出しました

---
+++ Assets/test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int[] array = { 1, 2, 3, 4, 5 };
+
+		for (int i = 0; i < 5; i++)
+		{
+			Debug.Log(array[i]);
+		}
+
+		for (int i = 4; i >=0; i--)
+		{
+			Debug.Log(array[i]);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/test1.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss
+{
+	private int mp = 53;          // mp
+
+	// magic関数
+	public void Magic(int mpsyouhi)
+	{//残りmp
+		int nokori = this.mp- mpsyouhi;
+
+		//関数の実行内容
+		if (nokori < 0)
+		{
+			Debug.Log("MPが足りないため魔法が使えない。");
+		}
+		else
+		{
+			Debug.Log("魔法攻撃をした。残りMPは"+nokori);
+		}
+
+	}
+
+}
+
+public class test1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Boss Mahou = new Boss();
+
+		for (int i = 1; i <= 11; i++)
+		{
+			Mahou.Magic(5*i);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
